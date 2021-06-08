a1d309f2837be0127ba14b0be94363448f983105
Violetta Zarytska <zarytska.vv@gmail.com>
2021-06-08 18:43:25 +0300

    img commit

--- modules/setBackgroudImage.js
+++ modules/setBackgroudImage.js
@@ -2,6 +2,6 @@ export const setBackgroungImg = (weatherType, period = 'day') => {
     const weatherField = document.querySelector('.weather-field');
     weatherType = weatherType.toLowerCase();
 
-    document.body.style.backgroundImage = `url('./images/background/${weatherType}-${period}.jpeg')`;
-    weatherField.style.backgroundImage = `url('./images/background/${weatherType}-${period}.jpeg')`;
+    document.body.style.backgroundImage = `url('./images/${weatherType}-${period}.jpeg')`;
+    weatherField.style.backgroundImage = `url('./images/${weatherType}-${period}.jpeg')`;
 }
